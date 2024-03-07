c4a0dc0097a637a7835e5d2ec68bbf75e61c45b7
vladakin4105 <vldpopescu4105@yahoo.com>
2024-03-07 10:54:11 +0200

    suggestive variable names

--- lab2.c
+++ lab2.c
@@ -1,16 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int function(int x){
+int reducere_2(int x){
 	x=x-2;
 	return x;
 }
 
 int main(){
-	int a=30, i=0;
+	int num=30, i=0;
 
-	while (a>0){
-		a=function(a);
+	while (num>0){
+		num=reducere_2(num);
 		i++;
 	}
 	printf("%d", i);
